c9e67070f7cabe44ce2114b4c514643241e51be8
Shawn M <forgeforza@outlook.com>
2016-11-10 23:54:04 -0600

    intial commit json non functional

---
+++ quiz.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+function populatePage () {
+  // Loop over the inventory and populate the page
+ 	 
+
+ 	//for (i = 0; i < inventory.length; i ++) {
+ 	 	inventory.toString() 
+ 	}
+
+
+
+  // Now that the DOM is loaded, establish all the event listeners needed
+  CarLot.activateEvents();
+}
+
+// Load the inventory and send a callback function to be
+// invoked after the process is complete
+CarLot.loadInventory();
+
+
+
+
+
+
